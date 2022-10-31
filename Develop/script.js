0b09c7a1bb161c7051e641d51530b607bd5ba499
MightyHero <115051723+MightyHero@users.noreply.github.com>
2022-10-31 16:44:44 -0500

    add a funtion to generate password and added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var capAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var specialCharacter = "!@#$%&*?";
+
+function generatePassword() {
+  var passwordLength = parseInt(prompt("How long do you want you password? "))
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var alphabet = confirm("Do you want to include lowercase letters? ");
+    var capAlphabet = confirm("Do yo wasnt to include uppercase letters? ");
+    var number = confirm("Do you want to include numbers?" );
+    var specialCharacter = confirm("Do you want to include special characters?" );
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
