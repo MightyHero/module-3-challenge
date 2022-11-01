8909ce07038a327167222278586ab5a65cdf6ab4
MightyHero <115051723+MightyHero@users.noreply.github.com>
2022-10-31 22:10:02 -0500

    added variable container with variables for password

--- Develop/script.js
+++ Develop/script.js
@@ -8,15 +8,37 @@ var specialCharacter = "!@#$%&*?";
 function generatePassword() {
   var passwordLength = parseInt(prompt("How long do you want you password? "))
   if (passwordLength >= 8 && passwordLength <= 128) {
-    var alphabet = confirm("Do you want to include lowercase letters? ");
-    var capAlphabet = confirm("Do yo wasnt to include uppercase letters? ");
-    var number = confirm("Do you want to include numbers?" );
-    var specialCharacter = confirm("Do you want to include special characters?" );
+    var alphabetConfirmation = confirm("Do you want to include lowercase letters? ");
+    var capAlphabetConfirmation = confirm("Do yo wasnt to include uppercase letters? ");
+    var numberConfirmation = confirm("Do you want to include numbers?" );
+    var specialCharacterConfirmation = confirm("Do you want to include special characters?" );
+    console.log(alphabetConfirmation);
   } else {
     alert(
       "The user's password must be at least 8 characters long and no more than 128"
     );
     return;
+    }
+    var passwordCharacterContainer = [];
+    if (alphabetConfirmation) {
+      passwordCharacterContainer = passwordCharacterContainer.concat(
+        alphabet.split("")
+      );
+    }
+    if (capAlphabetConfirmation) {
+      passwordCharacterContainer = passwordCharacterContainer.concat(
+        capAlphabet.split("")
+      );
+    }
+    if (numberConfirmation) {
+      passwordCharacterContainer = passwordCharacterContainer.concat(
+        number.split("")
+      );
+    }
+    if (specialCharacterConfirmation) {
+      passwordCharacterContainer = passwordCharacterContainer.concat(
+        specialCharacter.split("")
+      );
     }
 }
 
